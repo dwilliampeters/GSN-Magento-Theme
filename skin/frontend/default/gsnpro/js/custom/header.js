75c3da5f030548384753d25203726029bdaaa73f
danp <spartdan@live.co.uk>
2014-11-21 10:41:52 +0000

    Stuff :S
    Forgot to commit, forgot what I did! Some changes! :)

--- skin/frontend/default/gsnpro/js/custom/header.js
+++ skin/frontend/default/gsnpro/js/custom/header.js
@@ -1,9 +1,23 @@
 jQuery(function () {
-	"use strict";
-    
-    jQuery('.btn-search').on('click', function(e) {
-        e.preventDefault();
-        jQuery('.form-search').toggleClass('active');
-    });
+  "use strict";
+
+  jQuery('.btn-search').on('click', function(e) {
+    e.preventDefault();
+    jQuery('.form-search').toggleClass('active');
+  });
+  
+  jQuery('#nav-primary > ul > li > a').on('mouseenter', function(e) {
+    e.preventDefault();
+    if ( jQuery(this).parent().hasClass('open') ) {
+      jQuery(this).parent().removeClass('open');
+    } else {
+      jQuery('#nav-primary > ul > li').removeClass('open');
+      jQuery(this).parent().toggleClass('open');
+    }
+  });
+  
+  jQuery('#nav-primary > ul > li > ul').on('mouseleave', function(e) {
+    jQuery(this).parent().removeClass('open');
+  });
     
 });
